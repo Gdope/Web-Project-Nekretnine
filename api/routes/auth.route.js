301c8072f30a8e7b9e279c82b1cffa0a134375d2
Gdope <snedeljkovic2003@gmail.com>
2024-07-28 00:17:46 +0200

    Dodaj signout funkcionalnost

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,17 @@
 import express, { Router } from "express";
-import { google, signin, signup } from "../controllers/auth.controller.js";
+import {
+  google,
+  signin,
+  signOut,
+  signup,
+} from "../controllers/auth.controller.js";
+import { sign } from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signOut);
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,12 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import {
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
@@ -84,6 +90,36 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === fals) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -148,8 +184,15 @@ export default function Profile() {
       {loading && <p className="text-center">Updating...</p>}
       {error && <p className="text-red-700 text-center">{error}</p>}
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete account
+        </span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
       <p className="text-red-700 mt-5 ">{error ? error : ""} </p>
       <p className="text-green-700 mt-5">
